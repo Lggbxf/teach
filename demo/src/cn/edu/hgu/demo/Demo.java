22c8e342b247d81a85552435119b930f7e2a06eb
yp <lggbxf@163.com>
2018-10-29 15:58:05 +0800

    insert demo project

---
+++ demo/src/cn/edu/hgu/demo/Demo.java
@@ -0,0 +1,18 @@
+package cn.edu.hgu.demo;
+
+import net.sf.json.JSONObject;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Json json = new Json();
+		String str = json.JsonArray();
+		System.out.println("Demo.class:");
+		System.out.println(str);
+		JSONObject jsonObject = new JSONObject();
+        //JSONArray jsonArray = jsonObject.getJSONArray("data");
+		/*json
+		System.out.println("Demo.class:");*/
+		
+	}
+}
